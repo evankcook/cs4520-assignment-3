df5e816f470df08e4da1497174bd24aed3b85182
Evan Cook <cook.evank@gmail.com>
2024-02-28 18:15:14 -0500

    complete mvvm with viewmodelprovider

--- app/src/main/java/com.cs4520.assignment3/mvvm/CalculatorViewModel.kt
+++ app/src/main/java/com.cs4520.assignment3/mvvm/CalculatorViewModel.kt
@@ -3,11 +3,51 @@ package com.cs4520.assignment3.mvvm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.cs4520.assignment3.CalculatorModel
+import com.cs4520.assignment3.CalculatorModelImpl
 
 class CalculatorViewModel : ViewModel() {
 
     private val _result = MutableLiveData<String>()
-    val githubResponseData : LiveData<String> = _result
+    val result : LiveData<String> = _result
+    private val _error = MutableLiveData<String>()
+    val error : LiveData<String> = _error
+    private val model : CalculatorModel = CalculatorModelImpl()
+
+    fun onAddButtonClick(val1: Double?, val2: Double?) {
+        if (val1 == null || val2 == null) {
+            _error.value = "All inputs must be present to add."
+        } else {
+            _result.value = model.add(val1, val2).toString()
+        }
+    }
+
+    fun onSubtractButtonClick(val1: Double?, val2: Double?) {
+        if (val1 == null || val2 == null) {
+            _error.value = "All inputs must be present to subtract."
+        } else {
+            _result.value = model.subtract(val1, val2).toString()
+        }
+    }
+
+    fun onMultiplyButtonClick(val1: Double?, val2: Double?) {
+        if (val1 == null || val2 == null) {
+            _error.value = "All inputs must be present to multiply."
+        } else {
+            _result.value = model.multiply(val1, val2).toString()
+        }
+    }
+
+    fun onDivisionButtonClick(val1: Double?, val2: Double?) {
+        if (val1 == null || val2 == null) {
+            _error.value = "All inputs must be present to perform division."
+        } else if (val2 == 0.0) {
+            _error.value = "Cannot divide by zero."
+        } else {
+            _result.value = model.division(val1, val2).toString()
+        }
+    }
+
 
 
 
--- app/src/main/java/com.cs4520.assignment3/mvvm/MVVMFragment.kt
+++ app/src/main/java/com.cs4520.assignment3/mvvm/MVVMFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.cs4520.assignment3.databinding.MvvmFragmentBinding
+import com.cs4520.assignment3.mvp.CalculatorPresenter
 
 class MVVMFragment : Fragment() {
     private var _binding: MvvmFragmentBinding? = null
     private val binding get() = _binding!!
+    private var viewModel: CalculatorViewModel? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,5 +28,60 @@ class MVVMFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[CalculatorViewModel::class.java]
+
+        binding.addButton.setOnClickListener {
+            viewModel!!.onAddButtonClick(binding.firstInput.text.toString().toDoubleOrNull(),
+                binding.secondInput.text.toString().toDoubleOrNull())
+            this.clearAllInputs()
+        }
+        binding.subtractButton.setOnClickListener {
+            viewModel!!.onSubtractButtonClick(binding.firstInput.text.toString().toDoubleOrNull(),
+                binding.secondInput.text.toString().toDoubleOrNull())
+            this.clearAllInputs()
+        }
+        binding.multiplyButton.setOnClickListener {
+            viewModel!!.onMultiplyButtonClick(binding.firstInput.text.toString().toDoubleOrNull(),
+                binding.secondInput.text.toString().toDoubleOrNull())
+            this.clearAllInputs()
+        }
+        binding.divisionButton.setOnClickListener {
+            viewModel!!.onDivisionButtonClick(binding.firstInput.text.toString().toDoubleOrNull(),
+                binding.secondInput.text.toString().toDoubleOrNull())
+            this.clearAllInputs()
+        }
+        this.initObserver()
     }
+
+    private fun initObserver() {
+
+        viewModel?.result?.observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                if (!binding.result.isVisible) {
+                    binding.result.visibility = View.VISIBLE
+                }
+                binding.result.text = it
+            } else {
+                Toast.makeText(requireContext(), "Error Calculating Result", Toast.LENGTH_LONG)
+                    .show()
+            }
+        })
+
+        viewModel?.error?.observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                binding.result.visibility = View.GONE
+            } else {
+                Toast.makeText(requireContext(), "An Error occurred...", Toast.LENGTH_LONG)
+                    .show()
+            }
+        })
+
+    }
+
+    private fun clearAllInputs() {
+        binding.firstInput.text.clear()
+        binding.secondInput.text.clear()
+    }
+
 }
